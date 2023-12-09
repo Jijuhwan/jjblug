a5fd02937c0267200d3bc012d3a4bdea415d06e9
jjhw <otl9882@naver.com>
2023-12-10 03:10:34 +0900

    Initial commit

---
+++ src/main/java/com/jjapps/jjblug/JjblugApplication.java
@@ -0,0 +1,13 @@
+package com.jjapps.jjblug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjblugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjblugApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jjapps/jjblug/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.jjapps.jjblug;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
